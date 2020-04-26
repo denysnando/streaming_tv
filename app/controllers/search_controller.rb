2dd300bfd9b6cf8c191e88687af64bebe8409360
Denys Santos <denysnando@gmail.com>
2020-04-26 16:51:40 -0300

    added routes to seasons and movies

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class SearchController < ApplicationController
+  def search_movies
+    @movies = Movie.all.order(:created_at)
+
+    render json: @movies, each_serializer: MovieSerializer
+  end
+
+  def search_seasons
+    @seasons = Season.all.order(:created_at)
+
+    render json: @seasons, each_serializer: SeasonSerializer
+  end
+
+  def search_all
+    @seasons = Season.all.order(:created_at)
+    @movies = Movie.all.order(:created_at)
+
+    render json: {
+      movies: ActiveModelSerializers::SerializableResource.new(@movies, each_serializer: MovieSerializer),
+      seasons: ActiveModelSerializers::SerializableResource.new(@seasons, each_serializer: SeasonSerializer)
+    }
+  end
+end
