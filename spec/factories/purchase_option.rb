a761a8dcfd94139919261b457bab3914bd912072
Denys Santos <denysnando@gmail.com>
2020-04-26 20:13:59 -0300

    added specs to purchase_option_serializers achieve 100% coverage

--- spec/factories/purchase_option.rb
+++ spec/factories/purchase_option.rb
@@ -7,4 +7,14 @@ FactoryBot.define do
     purchaseable_id { find_or_create(:movie) }
     purchaseable_type { 'Movie' }
   end
+
+  trait :season do
+    purchaseable_id { find_or_create(:season) }
+    purchaseable_type { 'Season' }
+  end
+
+  trait :quality_sd do
+    quality { 'SD' }
+    price { Faker::Number.decimal(1) }
+  end
 end
--- spec/serializers/purchase_option_serializer_spec.rb
+++ spec/serializers/purchase_option_serializer_spec.rb
@@ -3,20 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseOptionSerializer, type: :serializer do
-  let(:purchase_option) { create(:purchase_option) }
-
-  subject(:serializer) { described_class.new(purchase_option) }
+  let(:purchase_option_movie) { create(:purchase_option) }
+  let(:purchase_option_seasion) { create(:purchase_option, :season) }
+  let!(:episode) { create(:episode) }
 
   describe '#attributes' do
-    it 'return the list of expected attributes' do
-      expect(serializer.as_json[:id]).to eq(purchase_option.id)
-      expect(serializer.as_json[:quality]).to eq(purchase_option.quality)
-      expect(serializer.as_json[:price]).to eq(purchase_option.price)
-      expect(serializer.as_json[:created_at]).to eq(purchase_option.created_at.strftime('%d/%m/%Y %H:%M:%S'))
-      expect(serializer.as_json[:purchaseable][:id]).to eq(purchase_option.purchaseable.id)
-      expect(serializer.as_json[:purchaseable][:title]).to eq(purchase_option.purchaseable.title)
-      expect(serializer.as_json[:purchaseable][:plot]).to eq(purchase_option.purchaseable.plot)
-      expect(serializer.as_json[:purchaseable][:created_at]).to eq(purchase_option.purchaseable.created_at.strftime('%d/%m/%Y %H:%M:%S'))
+    it 'return the list of expected attributes to movies' do
+      serializer = described_class.new(purchase_option_movie)
+
+      expect(serializer.as_json[:id]).to eq(purchase_option_movie.id)
+      expect(serializer.as_json[:quality]).to eq(purchase_option_movie.quality)
+      expect(serializer.as_json[:price]).to eq(purchase_option_movie.price)
+      expect(serializer.as_json[:created_at]).to eq(purchase_option_movie.created_at.strftime('%d/%m/%Y %H:%M:%S'))
+      expect(serializer.as_json[:purchaseable][:id]).to eq(purchase_option_movie.purchaseable.id)
+      expect(serializer.as_json[:purchaseable][:title]).to eq(purchase_option_movie.purchaseable.title)
+      expect(serializer.as_json[:purchaseable][:plot]).to eq(purchase_option_movie.purchaseable.plot)
+      expect(serializer.as_json[:purchaseable][:created_at]).to eq(purchase_option_movie.purchaseable.created_at.strftime('%d/%m/%Y %H:%M:%S'))
+    end
+
+    it 'return the list of expected attributes to movies' do
+      serializer = described_class.new(purchase_option_seasion)
+
+      expect(serializer.as_json[:id]).to eq(purchase_option_seasion.id)
+      expect(serializer.as_json[:quality]).to eq(purchase_option_seasion.quality)
+      expect(serializer.as_json[:price]).to eq(purchase_option_seasion.price)
+      expect(serializer.as_json[:created_at]).to eq(purchase_option_seasion.created_at.strftime('%d/%m/%Y %H:%M:%S'))
+      expect(serializer.as_json[:purchaseable][:id]).to eq(purchase_option_seasion.purchaseable.id)
+      expect(serializer.as_json[:purchaseable][:title]).to eq(purchase_option_seasion.purchaseable.title)
+      expect(serializer.as_json[:purchaseable][:plot]).to eq(purchase_option_seasion.purchaseable.plot)
+      expect(serializer.as_json[:purchaseable][:created_at]).to eq(purchase_option_seasion.purchaseable.created_at.strftime('%d/%m/%Y %H:%M:%S'))
+      expect(serializer.as_json[:purchaseable][:episodes].first.id).to eq(episode.id)
+      expect(serializer.as_json[:purchaseable][:episodes].first.title).to eq(episode.title)
+      expect(serializer.as_json[:purchaseable][:episodes].first.plot).to eq(episode.plot)
+      expect(serializer.as_json[:purchaseable][:episodes].first.number).to eq(episode.number)
     end
   end
 end
